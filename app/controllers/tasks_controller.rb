30f31edf1f53fdd9d82d008ea0ae03767496f6b9
arturo quiroz <r2inlondon@gmail.com>
2021-02-10 18:40:43 -0600

    checkbox in edit and destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,10 +27,15 @@ class TasksController < ApplicationController
     redirect_to tasks_path(@tasks)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
-
 end
